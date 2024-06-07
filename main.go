65e4435813b2aa97b3bc5c267aeb0828d2bdce4a
nklkarthi <nklkarthi@gmail.com>
2024-06-07 14:08:36 +0800

    Add main.go to handle the requests using mux

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"productservice/handlers"
+	"productservice/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/login", handlers.Login).Methods("POST")
+	router.HandleFunc("/api/products", handlers.GetProducts).Methods("GET")
+	router.HandleFunc("/api/products", handlers.AddProduct).Methods("POST")
+	router.HandleFunc("/api/products/{id:[0-9]+}", handlers.GetProduct).Methods("GET")
+
+	router.Use(middleware.JwtAuthentication) // JWT middleware
+
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
